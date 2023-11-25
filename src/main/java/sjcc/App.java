840d2c9309f4c515507de0ef7a2fa0adab2b1709
Venus Dowoang <121656507+Innominate-Majesty@users.noreply.github.com>
2023-11-24 23:33:00 -0800

    created a list of all the possible Switch cases

--- src/main/java/sjcc/App.java
+++ src/main/java/sjcc/App.java
@@ -1,13 +1,58 @@
 package sjcc;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner userInput = new Scanner(System.in);
+        boolean quit = false;
+
+        while (!quit) {
+            System.out.println("Enter a command: ");
+            String input = userInput.nextLine();
+
+            switch (input) {
+                case "l":
+                    //listing all the pharaohs
+                    listPharaohs();
+                    break;
+                case "d":
+                    //displaying information for a specific Egyptian pharoah
+                    System.out.println("Etner the ID of the pharaoh: ");
+                    String pharaohID = userInput.nextLine();
+                    displayPharaohInfo(pharaohID);
+                    break;
+                case "p":
+                    //listing all the pyramids
+                    listPyramids();
+                    break;
+                case "s":
+                    //display a specific pyramid
+                    System.out.println("Etner the pyramid ID: ");
+                    String pyramidID = userInput.nextLine();
+                    displayPyramidInfo(pyramidID);
+                    break;
+                case "r":
+                    //dispaying a list of requested pyramids (without duplicates)
+                    displayRequestedPyramids();
+                    break;
+                case "q":
+                    //quitting
+                    quit = true;
+                    break;
+                default:
+                    //Handling invalid commands
+                    System.out.println("Please enter a valid command!");
+                    break;
+            }
+        }
+
+        userInput.close();
+        System.out.println("Goodbye!");
     }
+    
+    
 }
