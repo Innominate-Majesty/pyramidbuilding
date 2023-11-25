d6441d9fbaf7cf3ff8d60f0f2ca022487790cd3a
Venus Dowoang <121656507+Innominate-Majesty@users.noreply.github.com>
2023-11-25 02:24:28 -0800

    attempting to connect the path between the resources and the App.java class

--- src/main/java/sjcc/App.java
+++ src/main/java/sjcc/App.java
@@ -2,9 +2,12 @@ package sjcc;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -13,8 +16,8 @@ import java.util.Set;
 public class App 
 {
 
-    private static final String PHARAOHS_JSON_PATH = "pharaoh.json";
-    private static final String PYRAMIDS_JSON_PATH = "pyramid.json";
+    private static final String PHARAOHS_JSON_PATH = "/pharaoh.json";
+    private static final String PYRAMIDS_JSON_PATH = "/pyramid.json";
     private static Set<String> requestedPyramids = new HashSet<>();
     public static void main( String[] args )
     {
@@ -36,8 +39,7 @@ public class App
                     String pharaohID = userInput.nextLine().trim();
                     if (!pharaohID.matches("\\d+")) {
                         System.out.println("Invalid. Please enter the correct ID");
-                    }
-                    else {
+                    } else {
                         displayPharaohInfo(pharaohID);
                     }
                     break;
@@ -51,8 +53,7 @@ public class App
                     String pyramidID = userInput.nextLine().trim();
                     if (!pyramidID.matches("\\d+")) {
                         System.out.println("Invalid ID. Please enter the correct ID");
-                    }
-                    else {
+                    } else {
                         displayPyramidInfo(pyramidID);
                     }
                     break;
@@ -75,9 +76,18 @@ public class App
         System.out.println("Goodbye!");
     }
     
+    private static String readFileFromClasspath(String path) throws IOException {
+        InputStream inputStream = App.class.getResourceAsStream(path);
+        if (inputStream == null) {
+            throw new IOException("Resource not found! " + path);
+        }
+        return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+
+    }
+    
     private static void listPharaohs() {
         try {
-            String data = new String(Files.readAllBytes(Paths.get(PHARAOHS_JSON_PATH)));
+            String data = readFileFromClasspath(PHARAOHS_JSON_PATH);
             JSONArray pharaohs = new JSONArray(data);
 
             if (pharaohs.isEmpty()) {
@@ -97,7 +107,7 @@ public class App
 
     private static void displayPharaohInfo(String pharaohID) {
         try {
-            String data = new String(Files.readAllBytes(Paths.get(PHARAOHS_JSON_PATH)));
+            String data = readFileFromClasspath(PHARAOHS_JSON_PATH);
             JSONArray pharaohs = new JSONArray(data);
 
             boolean found = false;
@@ -122,7 +132,7 @@ public class App
     
     private static void listPyramids() {
         try {
-            String data = new String(Files.readAllBytes(Paths.get(PYRAMIDS_JSON_PATH)));
+            String data = readFileFromClasspath(PYRAMIDS_JSON_PATH);
             JSONArray pyramids = new JSONArray(data);
 
             for (int i = 0; i < pyramids.length(); i++) {
@@ -138,8 +148,8 @@ public class App
 
     private static void displayPyramidInfo(String pyramidID) {
         try {
-            String data = new String(Files.readAllBytes(Paths.get(PYRAMIDS_JSON_PATH)));
-            JSONArray pyramids = new JSONArray();
+            String data = readFileFromClasspath(PYRAMIDS_JSON_PATH);
+            JSONArray pyramids = new JSONArray(data);
 
             for (int i = 0; i < pyramids.length(); i++) {
                 JSONObject pyramid = pyramids.getJSONObject(i);
@@ -157,7 +167,7 @@ public class App
 
     private static void displayRequestedPyramids() {
         try {
-            String data = new String(Files.readAllBytes(Paths.get(PYRAMIDS_JSON_PATH)));
+            String data = readFileFromClasspath(PYRAMIDS_JSON_PATH);
             JSONArray pyramids = new JSONArray(data);
 
             for (int i = 0; i < pyramids.length(); i++) {
